5f89a5ee50f6d8c3a5dfeb0dc047256fee9c92ca
Rita <rita.yeghiazarya.98@mail.ru>
2021-05-15 21:35:23 +0400

    project

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require("gulp");
-//Arus
+const autoprefixer = require("gulp-autoprefixer");
+const cssMinify = require("gulp-css-minify");
 const htmlmin = require("gulp-htmlmin");
-
+ const autoprefixer = require("gulp-autoprefixer");
+//Arus
 gulp.task("minify", () => {
   return gulp
     .src("Test/**/*.html")
@@ -10,9 +12,23 @@ gulp.task("minify", () => {
 });
 
 //Rita
-const cssMinify = require('gulp-css-minify');
-gulp.task('minifyCss', function () {
-  return gulp.src('Test/**/*.css')
+
+gulp.task("minifyCss", function () {
+  return gulp
+    .src("Test/**/*.css")
     .pipe(cssMinify())
-    .pipe(gulp.dest('./public/css'));
-});
+    .pipe(gulp.dest("./public/css"))
+
+})
+
+// gulp.task("css_style",function(){
+//   return gulp 
+//   .src("public/css/**/*.css")
+//   .pipe(autoprefixer({
+//     browsers: ['last 2 versions'],
+//     cascade:false
+
+// }))
+// .pipe(gulp.dest('prefix'))
+//  })
+
