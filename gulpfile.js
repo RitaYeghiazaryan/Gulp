dcc57efe1b6a40da28a9942a6a97b067d7f653be
Lauraa <laurabadalyan877@gmail.com>
2021-05-16 01:03:30 +0400

    Concat css

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cssMinify = require("gulp-css-minify");
 const htmlmin = require("gulp-htmlmin");
- const autoprefixer = require("gulp-autoprefixer");
+const concatCss = require('gulp-concat-css');
+
 //Arus
 gulp.task("minify", () => {
   return gulp
@@ -20,6 +21,32 @@ gulp.task("minifyCss", function () {
     .pipe(gulp.dest("./public/css"))
 
 })
+
+// Laura
+// npm install --save-dev gulp-concat-css
+gulp.task('cssconcat', function () {
+  return gulp.src([
+  'Test/10/*.css',
+  'Test/block1/css/*.css',
+  'Test/Block3/*.css',
+  'Test/block4/css/*.css',
+  'Test/Block6/*.scss',
+  'Test/block13/*.css',
+  'Test/block14/*.css',
+  'Test/block15/Css/*.css',
+  'Test/block2/*.css',
+  'Test/block12/css/*.css',
+  'Test/part5/*.css',
+  'Test/part7/*.css',
+  'Test/part8/*.css',
+  'Test/part9/*.css',
+  'Test/part11/*.css',
+  'Test/part16/*.scss'
+])
+  //gulp.src(['Test/**/*.css','Test/**/**/css/*.css'])
+    .pipe(concatCss("all.css"))
+    .pipe(gulp.dest('cssconcat/'));
+});
 
 // gulp.task("css_style",function(){
 //   return gulp 
